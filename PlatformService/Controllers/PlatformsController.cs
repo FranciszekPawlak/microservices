83dba2a31ebfc79d872dcb48156b56f015b599aa
Franciszek Pawlak <box@franciszekpawlak.pl>
2023-07-22 15:23:44 +0200

    init

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IPlatformRepo _repository;
+        public  PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(_repository.GetAllPlatforms()));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platform = _mapper.Map<PlatformReadDto>(_repository.GetPlatformById(id));
+            if(platform == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(platform);
+        }
+
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platform)
+        {
+            var platformToCreate = _mapper.Map<Platform>(platform);
+             _repository.CreatePlatform(platformToCreate);
+            _repository.SaveChanges();
+
+            var createdPlatform = _mapper.Map<PlatformReadDto>(platformToCreate);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = createdPlatform.Id}, createdPlatform);
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,31 @@
+﻿namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(WebApplication app)
+        {
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                SeeData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeeData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                context.Platforms.AddRange(
+                    new Models.Platform() { Name = "Item 1", Publisher = "Publisher 1", Cost = "Free" },
+                    new Models.Platform() { Name = "Item 2", Publisher = "Publisher 2", Cost = "Free" },
+                    new Models.Platform() { Name = "Item 3", Publisher = "Publisher 3", Cost = "Free" }
+                    );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("we have data");
+            }
+        }
+    }
+}
